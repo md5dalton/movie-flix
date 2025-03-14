087275fe4bc5d72e3e103d0baf82a48a76d103f7
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-14 15:55:33 +0200

    Create api.ts

---
+++ service/api.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_TMDB_API_KEY,
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_TOKEN}`,
+    }
+}
+
+
+export const moviesApi = createApi({
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: TMDB_CONFIG.BASE_URL,
+        prepareHeaders: headers => {
+            headers.set("accept", "application/json")
+            headers.set("Authorization", `Bearer ${process.env.EXPO_PUBLIC_TMDB_API_TOKEN}`)
+            return headers
+        }
+    }),
+    endpoints: (builder) => ({
+        fetchMovies: builder.query({
+            query: () => "/discover/movie?sort_by=popularity.desc",
+        }),
+    }),
+})
+
+export const { useFetchMoviesQuery } = moviesApi
