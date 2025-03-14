77920c62a559d866e61e7d85ad6e670a1079b198
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2025-03-14 11:29:07 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"
+import "./global.css"
 
-export default function RootLayout() {
-  return <Stack />;
-}
+export default () => <Stack />
